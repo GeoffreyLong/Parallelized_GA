95765ba629436f8d7805e69e42a7625cbd7f955b
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2015-12-09 14:19:29 -0500

    Add initial class

--- main.cpp
+++ main.cpp
@@ -4,35 +4,66 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
 
+class City {
+  public:
+    City(int nodeNum, double x, double y);
+    int getNodeNum() {return nodeNum;}
+    double getX() {return x;}
+    double getY() {return y;}
+  private:
+    int nodeNum;
+    double x;
+    double y;
+};
 
 
+City::City(int nodeNum, double x, double y){
+  this->nodeNum = nodeNum;
+  this->x = x;
+  this->y = y;
+}
 
 int main(){
-  ifstream inFile("DataSets/eil51.tsp");
+  // Use knowledge of the dataset size...
+  ifstream inFile("DataSets/eil51.tsp"); int size = 51;
+  
+  
   if (!inFile) {
     cerr << "Input file not found." << endl;
     return -1;
   }
 
-  
+  vector<City> cities;
 
-  std::string line;
+  string line;
   while (std::getline(inFile, line)){
     if (line.empty()) continue;
-    std::istringstream iss(line);
-    double a, b, c;
+    istringstream iss(line);
+    int nodeNum;
+    double x, y;
+
 
     // Not entirely sure that this will work for all files...
     // If the input stream does not have three args it is not a tour location
-    if (!(iss >> a >> b >> c)) { continue; }
+    if (!(iss >> nodeNum >> x >> y)) { continue; }
+
+
+    cities.push_back(City(nodeNum, x, y));
 
     // Check output
-    // cout << a << " " << b << " " << c << std::endl;
+    //cout << nodeNum << " " << x << " " << y << endl;
   }
 
+
+  for (unsigned int i = 0; i < cities.size(); i++){
+    City city = cities.at(i);
+    cout << city.getNodeNum() << " " << city.getX() << " " << city.getY() << endl;
+  }
+  
    return 0;
 }
