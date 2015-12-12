57c5d7a6c7c0886859cb9bf580b6ba665ef9d06e
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2015-12-12 02:51:01 -0500

    Minor bug fix

--- parallel_GA.cpp
+++ parallel_GA.cpp
@@ -74,11 +74,9 @@ int main(){
   int threads[36] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
     21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36};
 
-double bestSpeedup = 0;
-
   double sequentialTime = 0;
   for (int pop = 0; pop < 1; pop ++){
-    for (int iter = 3; iter < 1; iter ++){
+    for (int iter = 0; iter < 1; iter ++){
       for (int num_threads = 0; num_threads < 36; num_threads ++){
         int numT = threads[num_threads];
         //int populationSize = sizes[pop];
